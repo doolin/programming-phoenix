ac856041f8420269bbf92745dca79a68b07b6751
dave doolin <david.doolin@gmail.com>
2017-06-26 08:17:48 -0700

    p. 49 showing a user, errata
    I had planned to a bit more than this today, but
    it turns out I spent some time chasing down a bug
    in the book. There is a pointer in the code with
    a link to the pragprog errata page.

--- rumbl/web/controllers/user_controller.ex
+++ rumbl/web/controllers/user_controller.ex
@@ -5,4 +5,11 @@ defmodule Rumbl.UserController do
     users = Repo.all(Rumbl.User)
     render conn, "index.html", users: users
   end
+
+  def show(conn, %{"id" => id}) do
+    # ERRATA: https://pragprog.com/titles/phoenix/errata#80257
+    # String.to_integer(id)
+    user = Repo.get(Rumbl.User, String.to_integer(id))
+    render conn, "show.html", user: user
+  end
 end
