26e837435195bd3a2dd954bae94aae817cf8edee
dave doolin <david.doolin@gmail.com>
2017-07-07 07:31:47 -0700

    p. 73 registration changeset
    The book has some accumulated errors with how changesets
    are defined. This noted on the web, but the recommended
    fixes don't work either, Phoenix has moved past them as
    well.
    
    This sort of thing isn't bad, per se. Fixing it will require
    doing some open-ended digging, which means for now this
    project is probably not suitable for daily incremental
    progress. Daily increments need to be 15-30 minutes in
    length, no longer.
    
    This kind of blows as I'd committed to working through the
    book on a regular schedule. I'll have to see about either
    taking time from something else to fix it, or try to work
    in the fixes after the usual morning commit.

--- rumbl/web/models/user.ex
+++ rumbl/web/models/user.ex
@@ -10,9 +10,37 @@ defmodule Rumbl.User do
     timestamps()
   end
 
+  # This seems to break the server.
+  """
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, ~w(name username), [])
     |> validate_length(:username, min: 1, max: 20)
+  end
+  """
+  # This was found on a github issues page.
+  def changeset(model, params \\ :invalid) do
+     model
+   |> cast(params, ~w(name username))
+   |> validate_required([:name, :username])
+   |> validate_length(:username, min: 1, max: 20)
+   |> unique_constraint(:username) # save the unique_constraint for last as it "hits" the DB. ;-)
+   end
+
+  def registration_changeset(model, params \\ :empty) do
+    model
+    |> changeset(params)
+    |> cast(params, ~w(password), [])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+        _ ->
+          changeset
+    end
   end
 end
